1c1952f0386f29e4ee521755df1ecf7d4a57dab1
that83 <that83@yahoo.com>
2024-06-18 23:22:05 +0700

    change gpt3.5 for TripAgent

--- trip_planner/trip_agents.py
+++ trip_planner/trip_agents.py
@@ -1,3 +1,5 @@
+import os
+
 from crewai import Agent
 from langchain_community.llms import OpenAI
 
@@ -5,6 +7,11 @@ from tools.browser_tools import BrowserTools
 from tools.calculator_tools import CalculatorTools
 from tools.search_tools import SearchTools
 
+llm = ChatOpenAI(openai_api_base=os.environ.get("OPENAI_API_BASE_URL", "https://api.openai.com/v1"),
+                        openai_api_key=os.environ.get("OPENAI_API_KEY"),
+                        temperature=0.1,                        
+                        model_name=os.environ.get("MODEL_NAME", "gpt-3.5-turbo"),
+                        top_p=0.3)
 
 class TripAgents():
 
@@ -18,6 +25,7 @@ class TripAgents():
             SearchTools.search_internet,
             BrowserTools.scrape_and_summarize_website,
         ],
+        llm=llm,
         verbose=True)
 
   def local_expert(self):
@@ -30,6 +38,7 @@ class TripAgents():
             SearchTools.search_internet,
             BrowserTools.scrape_and_summarize_website,
         ],
+        llm=llm,
         verbose=True)
 
   def travel_concierge(self):
@@ -44,4 +53,5 @@ class TripAgents():
             BrowserTools.scrape_and_summarize_website,
             CalculatorTools.calculate,
         ],
+        llm=llm,
         verbose=True)
