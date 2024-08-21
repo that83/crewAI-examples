78501d9c26e74ff75b3feb7874898e58b79a682f
that83 <that83@yahoo.com>
2024-08-21 08:27:22 +0700

    update task clearer

--- scraper/research_tasks.py
+++ scraper/research_tasks.py
@@ -7,11 +7,11 @@ class ResearchTasks():
   def identify_task(self, agent, request, specific_requirements):
     return Task(description=dedent(f"""
       Purpose of this process to scrape as much data as possible. The scraped data will be saved to file and analyzed in another application.
-      Step 0: Break down the requirements/request into searching keywords. Then, for each keywor, perform the following steps:
+      Step 0: Break down the requirements/request into searching keywords. Then, for each keyword, perform the following steps:
       Step 1: Search the internet and check the reliability based on the URLs of the search results.
-      Step 2: Pick the search results that seem reliable and objective (Skip any search results related to sales, advertising, or service offerings.)
-      Step 3: Use a tool to scrape the data of Step 2's urls and save it to a file.                             
-      Step 4: After obtaining the results, quickly check the "Snippet", think of additional useful requirements. Loop back to Step 0 until you can not think of any more keywords.
+      Step 2: Pick the search results that seem reliable and objective (Skip any related to sales, advertising, or service offerings.)
+      Step 3: Use tool to scrape the data of Step 2's url(s) and save it to a file.                             
+      Step 4: After obtaining the results, quickly check its "Snippet", think of additional useful requirements. Loop back to Step 0 until you can not think of any more keywords.
 
       Original Research Request: {request}
       Original Specific Requirements: {specific_requirements}
